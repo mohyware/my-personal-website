05f03ea5e8d2041a3728070e188f3caf09ee6918
mohyware <mohieelden50@gmail.com>
2025-06-10 16:30:08 +0300

    center profile section in mobiles

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,37 +47,37 @@ export default function RootLayout({
                             <div className="flex items-center justify-center">
                                 <div className="w-full max-w-3xl rounded-2xl p-8 md:p-12">
                                     {/* Profile Section */}
-                                    <div className="flex flex-col">
-                                        <div className="flex justify-between items-start">
-                                            <div className="flex flex-col items-center text-center mb-10">
-                                                <Image
-                                                    src={profilePictureLink}
-                                                    alt="Profile picture"
-                                                    width={80}
-                                                    height={80}
-                                                    className="rounded-full mb-4 object-cover"
-                                                />
-                                                <h1 className="text-2xl font-bold font-mono ">Mohy Elden</h1>
-                                                <p className="text-textLight dark:text-darkText mb-2">Software Engineer</p>
-                                                <div className="flex gap-4 mb-2">
-                                                    <a href={xLink} target="_blank" className='hover:text-gray-400' aria-label="X"><Twitter /></a>
-                                                    <a href={githubLink} target="_blank" className='hover:text-gray-400' aria-label="GitHub"><Github /></a>
-                                                    <a href={linkedinLink} target="_blank" className='hover:text-gray-400' aria-label="LinkedIn"><Linkedin /></a>
-                                                    <a href={emailLink} aria-label="Email" target="_blank" className='hover:text-gray-400' ><Mail /></a>
-                                                </div>
-                                                <div className="flex gap-2 items-center ">
-                                                    <a href={resumeLink} target="_blank"
-                                                        className="flex items-center gap-2 bg-gray-200 hover:bg-gray-300 px-3 py-1 mt-2 rounded-full text-gray-800">
-                                                        My Resume
-                                                    </a>
-                                                    <a
-                                                        href={resumeDownloadLink}
-                                                        download
-                                                        rel="noopener noreferrer">
-                                                        <Download className='hover:text-gray-400 mt-1' />
-                                                    </a>
-                                                </div>
+                                    <div className="flex flex-col md:flex-row md:justify-between md:items-start items-center">
+                                        <div className="flex flex-col items-center text-center md:mb-10 mb-4">
+                                            <Image
+                                                src={profilePictureLink}
+                                                alt="Profile picture"
+                                                width={80}
+                                                height={80}
+                                                className="rounded-full mb-4 object-cover"
+                                            />
+                                            <h1 className="text-2xl font-bold font-mono ">Mohy Elden</h1>
+                                            <p className="text-textLight dark:text-darkText mb-2">Software Engineer</p>
+                                            <div className="flex gap-4 mb-2">
+                                                <a href={xLink} target="_blank" className='hover:text-gray-400' aria-label="X"><Twitter /></a>
+                                                <a href={githubLink} target="_blank" className='hover:text-gray-400' aria-label="GitHub"><Github /></a>
+                                                <a href={linkedinLink} target="_blank" className='hover:text-gray-400' aria-label="LinkedIn"><Linkedin /></a>
+                                                <a href={emailLink} aria-label="Email" target="_blank" className='hover:text-gray-400' ><Mail /></a>
                                             </div>
+                                            <div className="flex gap-2 items-center ">
+                                                <a href={resumeLink} target="_blank"
+                                                    className="flex items-center gap-2 bg-gray-200 hover:bg-gray-300 px-3 py-1 mt-2 rounded-full text-gray-800">
+                                                    My Resume
+                                                </a>
+                                                <a
+                                                    href={resumeDownloadLink}
+                                                    download
+                                                    rel="noopener noreferrer">
+                                                    <Download className='hover:text-gray-400 mt-1' />
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div className='mb-6'>
                                             <ThemeSwitcher />
                                         </div>
                                     </div>
