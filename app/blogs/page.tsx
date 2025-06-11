5ba6aeea3d2ed9b4b355d88c92517d48e3e63072
mohyware <mohieelden50@gmail.com>
2025-06-11 08:18:36 +0300

    remove unused file

--- app/blogs/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Blogs() {
-    return (
-        <div>
-            <h1 className="text-2xl font-bold mb-4">Blogs</h1>
-            <p>This is the Blogs page.</p>
-        </div>
-    )
-} 
