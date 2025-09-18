d559d3d077e53be0f8c46691e7fc8e5fd10ff132
mohyware <mohieelden50@gmail.com>
2025-09-18 16:56:58 +0300

    update blogs style & categorize the new blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -24,7 +24,9 @@ export default async function Blog() {
             tag = "GSoC Blogs";
         }
 
-        if (post.categories?.[0] === "google-summer-of-code" || post.categories?.[1] === "google-summer-of-code" || post.categories?.[2] === "google-summer-of-code") {
+        if (post.categories?.[0] === "google-summer-of-code" || post.categories?.[1] === "google-summer-of-code" || post.categories?.[2] === "google-summer-of-code" ||
+            post.categories?.[0] === "side-project" || post.categories?.[1] === "side-project" || post.categories?.[2] === "side-project"
+        ) {
             tag = "Writing";
         }
 
@@ -37,17 +39,17 @@ export default async function Blog() {
             {Object.entries(groups).map(([tag, posts]) => (
                 <div key={tag} className="mb-12">
                     <h2 className="text-3xl font-bold mb-4 font-mono">{tag}</h2>
-                    <ul className="space-y-2">
+                    <ul className="space-y-3">
                         {posts.map(post => (
-                            <li key={post.guid} className="flex items-center">
+                            <li key={post.guid} className="items-baseline">
                                 <span className="mr-2 text-lg">•</span>
                                 <Link
                                     href={`/blog/${post.guid.split('/').pop()}`}
-                                    className="hover:text-gray-400 font-mono "
+                                    className="hover:text-gray-400 font-mono hyphens-auto break-words"
                                 >
                                     {post.title}
                                 </Link>
-                                <span className="ml-2 text-sm text-gray-400">
+                                <span className="ml-2 text-sm text-gray-400 whitespace-nowrap">
                                     {new Date(post.pubDate).toLocaleDateString('en-US', {
                                         year: 'numeric',
                                         month: 'long',
